b268a0895b6ab2a749a17eb8125aecf811067dc6
Yaswanth Rongala <rongalayaswanth@gmail.com>
2023-08-15 16:53:30 +0530

    initial comet

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,2 +1,18 @@
 import { createSlice } from "@reduxjs/toolkit";
-const initialState = {};
+const initialState = {
+  name: "",
+  email: "",
+  photo: "",
+};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    setUserLoginDetails: (state, action) => {
+      state.name = action.payload.name;
+      state.email = action.payload.email;
+      state.photo = action.payload.photo;
+    },
+  },
+});
